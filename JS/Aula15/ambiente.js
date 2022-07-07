975a68cbd30d4123fbc2829ff46cfbf7aebf2edc
BeaGabriela <biiagabrielaa@gmail.com>
2022-07-07 14:05:23 -0300

    estudos

---
+++ JS/Aula15/ambiente.js
@@ -0,0 +1,17 @@
+let num = [5,8,2,9,3]
+num[5] = 6;
+num.push(1)
+num.sort();
+console.log(num)
+console.log(`O Vetor tem ${num.length} posições`);
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+for(var i = 0; i<num.length(); i++){
+    console.log(num[i])
+}
+
+
+
+//Num.length() - Comprimento do vetor.
+//Num.sort() - Pega todos os valores e colocar em ordem crescente.
+
+
