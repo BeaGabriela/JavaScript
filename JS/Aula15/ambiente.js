657f8fd9aa2197f97b88705d9263cad1b4b887dd
BeaGabriela <biiagabrielaa@gmail.com>
2022-07-08 15:07:23 -0300

    estudos

--- JS/Aula15/ambiente.js
+++ JS/Aula15/ambiente.js
@@ -5,9 +5,16 @@ num.sort();
 console.log(num)
 console.log(`O Vetor tem ${num.length} posições`);
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-for(var i = 0; i<num.length(); i++){
-    console.log(num[i])
+let pos = num.indexOf(2)
+
+if(pos == -1){
+    console.log('Valor não encontrado')
+}else{
+console.log(`O valor 2 está na posição ${pos}`)
 }
+// for(let i = 0; i < num.length; i++){
+//     console.log(`Vetor ${num[i]}`)
+// }
 
 
 
