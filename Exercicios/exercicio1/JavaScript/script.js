576184b7e30a511b3d277a9a36e57fd2194c815a
BeaGabriela <biiagabrielaa@gmail.com>
2022-07-13 15:24:00 -0300

    estudos

--- Exercicios/exercicio1/JavaScript/script.js
+++ Exercicios/exercicio1/JavaScript/script.js
@@ -1,1 +1,18 @@
-window.alert('Olá!')
+function carregar(){
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+// var hora = data.getHours()
+var hora= 20
+msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora< 12){
+        //BOM DIA
+        img.src = './img/manha.png'
+    } else if (hora >= 12 && hora <= 18){
+        //Boa tarde
+        img.src = './img/tarde.png'
+    } else{
+        //Boa noite 
+        img.src = './img/noite .png'
+    }
+}
